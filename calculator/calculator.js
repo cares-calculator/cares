4b634abacdff6a2d1706b623007d24affd327dbc
Daniel Sim <daniel.ssq89@gmail.com>
2018-02-28 13:07:10 +0800

    Referece the inline template

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,21 +3,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
   Vue.component('inlineInfo', {
-    template:
-      `
-      <div>
-        <div style="display: inline">
-          <slot name="title"></slot>
-          <span class="inline-info-i"  @click="shown = !shown">
-            ⓘ
-          </span>
-        </div>
-        <transition name="expand">
-          <div v-if="shown" class="inline-info-style">
-            <slot></slot>
-          </div>
-        </transition>
-      </div>`,
+    template: '#inline-info-template',
     data: function () {
       return {
         shown: false,
