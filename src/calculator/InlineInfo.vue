dc368f783a97f1ad421b6d13d395525e9ac6068b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-03-03 04:32:39 +0000

    Deploy eileensim/cares to github.com/eileensim/cares.git:gh-pages

---
+++ src/calculator/InlineInfo.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <div style="display: inline">
+    <slot name="title"></slot>
+    <span class="inline-info-i"  @click="shown = !shown">
+      ⓘ
+    </span>
+  </div>
+  <transition name="expand">
+    <div v-if="shown" class="inline-info-style">
+      <slot></slot>
+    </div>
+  </transition>
+</div>
+</template>
+<script>
+export default {
+  data: function () {
+    return {
+      shown: false,
+    }
+  }
+}
+</script>
